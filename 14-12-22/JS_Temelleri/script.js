03ca6fd01360a57ce7d0c008159938b2cc19cfa2
Barbaros <barb.ciga@gmail.com>
2022-12-14 22:13:06 +0300

    JS-Essentials

---
+++ 14-12-22/JS_Temelleri/script.js
@@ -0,0 +1,51 @@
+// window.alert("S.A");
+// console.log("S.A");
+
+//yorum satırı bu şekilde yazılmaktadır.
+/*
+Çoklu
+Yorum
+Satırı
+*/
+
+/*Veri Tipleri
+String --> Metinsel İfadeler,
+Number --> Sayısal İfadeler,
+boolean --> True ya da False dönen karar ifadeleri
+object --> nesne,
+undefined --> tanımlanamayan veri tipi,
+null --> boş (henüz tanımlanmamış veriler)
+*/
+
+/*
+Değişken tanımlama ifadeleri
+var,
+let,
+const
+*/
+
+//Aşağıda değiştirilemez bir değişken tanımladık. Ve bu değişkene tanımladığımız anda bir değer ataması yaptık. Finalde de konsol ekranımızda bu değeri yazdırmak istedik.
+// const pi = 3.14;
+// console.log(pi);
+
+// var sayi = 5;
+// console.log(sayi);
+
+// sayi = 10;
+// console.log(sayi);
+//Type of kullanarak değişkenlerimizin tiplerini öğrenebiliyoruz.
+// console.log(typeof sayi);
+
+//String,number,boolean değerleri atayalım.
+// let name = "Yiğit";
+// var surname = "Paşa";
+// let age = 24;
+// var isADeveloper = true;
+
+// console.log(name, surname, age, isADeveloper);
+
+// window.prompt("Lütfen Adınızı Girin");
+
+//Kullanıcının veri girişi yapacağı basit bir kutucuk gösterimi sağlamak için prompt özelliği kullanılmaktadır.
+// var isim = prompt("İsminiz Nedir?");
+// console.log(isim);
