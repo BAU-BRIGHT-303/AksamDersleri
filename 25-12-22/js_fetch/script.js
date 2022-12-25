403244df413a3220490affc92c97dbd0358fb41a
Barbaros <barb.ciga@gmail.com>
2022-12-25 15:47:51 +0300

    fetch get post

--- 25-12-22/js_fetch/script.js
+++ 25-12-22/js_fetch/script.js
@@ -5,6 +5,12 @@ document
   .getElementById('calisanGetir')
   .addEventListener('click', calisanGetirFunc);
 
+document.getElementById('apiGetir').addEventListener('click', apiGetirFunc);
+
+document
+  .getElementById('postOlustur')
+  .addEventListener('submit', postOlusturfunc);
+
 function yaziGetirFunc() {
   //   console.log('Yazi Getirme Butonu Çalıştırıldı');
   //   fetch('./yazi.txt')
@@ -57,4 +63,70 @@ function calisanGetirFunc() {
       }
     })
     .catch((err) => console.log(err));
+}
+
+function apiGetirFunc() {
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       let output = '<h2>Postlar</h2>';
+  //       data.map(
+  //         (api) =>
+  //           (output += `
+
+  //         <div class="bg-dark text-light">
+  //         <h3>${api.title}</h3>
+  //         <p>${api.body}</p>
+  //         </div>
+  //         `)
+  //       );
+  //       cikti.innerHTML = output;
+  //     });
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((data) => {
+      let output = `<h2>Postlar</h2>
+      <div class="row">`;
+      data.map(
+        (api) =>
+          (output += `
+        <div class="col-md-3 my-5">
+        <div class="card" style="width: auto; height:300px">
+  <div class="card-body">
+    <h5 class="card-title">${api.title}</h5>
+    
+    <p class="card-text">${api.body}</p>
+    <a href="#" class="card-link">Detay</a>
+   
+  </div>
+</div>
+        
+        </div>
+        `)
+      );
+      cikti.innerHTML = output + '</div>';
+    });
+}
+
+function postOlusturfunc(e) {
+  e.preventDefault();
+  console.log('submit yapıldı');
+
+  let baslik = document.getElementById('title').value;
+  let govde = document.getElementById('body').value;
+  //   console.log(title, body);
+
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: baslik,
+      body: govde,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
 }
