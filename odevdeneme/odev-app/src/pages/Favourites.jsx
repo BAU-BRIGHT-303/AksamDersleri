e61b3c23ca297e537fdee7caf4e79c0137a0e658
ezgicaliskann <casezgi@gmail.com>
2023-01-17 20:26:09 +0300

    dfdf

--- odevdeneme/odev-app/src/pages/Favourites.jsx
+++ odevdeneme/odev-app/src/pages/Favourites.jsx
@@ -2,75 +2,74 @@ import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 import { AppContext } from "../context/GlobalContext";
 
 function Favourites() {
-  const {addFavourites, removeFavourites, favourites} = 
-  useContext(AppContext);
+  const { addFavourites, removeFavourites, favourites } =
+    useContext(AppContext);
 
-  const controlFavourites =(x) => {
-    const choice = favourites.some((a) => a.id ==x);
+  const controlFavourites = (x) => {
+    const choice = favourites.some((a) => a.id == x);
     return choice;
   };
 
   const navigate = useNavigate();
   return (
     <div>
-    <div className="container sayfa">
-      {favourites.length == 0 ? (
-        <div className="alert alert-danger my-5 m-auto" role="alert">
-          Favorilerinizde hiç ürün bulunmamaktadır.
-        </div>
-      ) : (
-        <div className="row ">
-          {favourites.map((record, index) => (
-            <div key={index} className="col-md-4 my-3 ">
-              <div
-                className="card m-auto shadow-lg p-3 mb-5 bg-body-tertiary rounded"
-                style={{ width: "22rem", height: "550px" }}
-              >
-                <img
-                  src={record.img}
-                  className="card-img-top m-auto"
-                  alt="..."
-                  style={{
-                    height: "300px",
-                    cursor: "pointer",
-                    width: "200px",
-                  }}
-                  onClick={() => navigate(`/plak/${record.id}`)}
-                />
-                <div className="card-body">
-                  <h6 className="card-title">{record.name}</h6>
-                  <p className="card-text">
-                    Tür: {record.album.slice(0, 50)}...
-                  </p>
-                  <p className="card-text">Yazarlar: {record.desc}</p>
-                  {controlFavourites(record.id) ? (
-                    <button
-                      className="btn btn-danger "
-                      onClick={() => removeFavourites(record.id)}
-                    >
-                      Remove from Favourites
-                    </button>
-                  ) : (
-                    <button
-                      className="btn btn-warning "
-                      onClick={() => addFavourites(record)}
-                    >
-                      Add to Favourites
-                    </button>
-                  )}
+      <div className="container sayfa">
+        {favourites.length == 0 ? (
+          <div className="alert alert-danger my-5 m-auto" role="alert">
+            Favorilerinizde hiç ürün bulunmamaktadır.
+          </div>
+        ) : (
+          <div className="row ">
+            {favourites.map((record, index) => (
+              <div key={index} className="col-md-4 my-3 ">
+                <div
+                  className="card m-auto shadow-lg p-3 mb-5 bg-body-tertiary rounded"
+                  style={{ width: "22rem", height: "550px" }}
+                >
+                  <img
+                    src={record.img}
+                    className="card-img-top m-auto"
+                    alt="..."
+                    style={{
+                      height: "300px",
+                      cursor: "pointer",
+                      width: "200px",
+                    }}
+                    onClick={() => navigate(`/plak/${record.id}`)}
+                  />
+                  <div className="card-body">
+                    <h6 className="card-title">{record.name}</h6>
+                    <p className="card-text">
+                      Tür: {record.album.slice(0, 50)}...
+                    </p>
+                    <p className="card-text">Yazarlar: {record.desc}</p>
+                    {controlFavourites(record.id) ? (
+                      <button
+                        className="btn btn-danger "
+                        onClick={() => removeFavourites(record.id)}
+                      >
+                        Remove from Favourites
+                      </button>
+                    ) : (
+                      <button
+                        className="btn btn-warning "
+                        onClick={() => addFavourites(record)}
+                      >
+                        Add to Favourites
+                      </button>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-      )}
+            ))}
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default Favourites;
