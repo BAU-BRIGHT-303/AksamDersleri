222a1dca463d078c7c70632e480f8510020c86d2
kerembstngl <kerembostanolu78@gmail.com>
2023-01-17 11:48:40 +0300

    1

---
+++ D&R/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+     <footer className="bg-light text-center text-lg-start mt-3 copyName">
+        <div className="text-center p-3 " style={{ backgroundColor: "#ffc107" }}>
+          © 2023 Copyright:
+          <a className=""> Kerem Bostanoğlu</a>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
