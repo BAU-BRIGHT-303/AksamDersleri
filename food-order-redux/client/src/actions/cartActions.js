02a0983288afbb77228d6f3e5e4fba4e4a958418
eneskrmnoglu <eneskrmnoglu98@gmail.com>
2023-01-24 13:56:22 +0300

    food-order-redux
    Food order sepet işlemleri için aksiyonlar ve reducerlar oluşturuldu. Sepet tasarımı yapıldı.

---
+++ food-order-redux/client/src/actions/cartActions.js
@@ -0,0 +1,37 @@
+export const addToCartAction = (menu, miktar, ozellik) => (dispatch) => {
+  var cartItem = {
+    ad: menu.ad,
+    _id: menu._id,
+    img: menu.img,
+    ozellik: menu.ozellik,
+    boyut: ozellik,
+    miktar: miktar,
+    fiyat: menu.fiyat,
+    desc: menu.desc,
+    kategori: menu.kategori,
+    fiyatlar: parseFloat(menu.fiyat[0][ozellik]) * miktar,
+  };
+  if (miktar > 0) {
+    dispatch({ type: "ADD_TO_CART", payload: cartItem });
+  }
+};
+
+export const removeFromCartAction = (id) => (dispatch) => {
+  dispatch({ type: "REMOVE_FROM_CART", payload: id });
+};
+
+export const updateCartItemAction =
+  (id, newQuantity) => (dispatch, getState) => {
+    const cartItems = getState().cartReducer.cartItems;
+    const itemToUpdate = cartItems.find((item) => item._id === id);
+    const updatedItem = {
+      ...itemToUpdate,
+      miktar: newQuantity,
+      fiyatlar:
+        parseFloat(itemToUpdate.fiyatlar / itemToUpdate.miktar) * newQuantity,
+    };
+    if (newQuantity > 0)
+      dispatch({ type: "UPDATE_CART_ITEM", payload: updatedItem });
+    else if (newQuantity === 0)
+      dispatch({ type: "REMOVE_FROM_CART", payload: id });
+  };
