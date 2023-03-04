03ffc5461b34c56847fd0058b7d4ec1472e35eae
Barbaros <barb.ciga@gmail.com>
2023-03-04 16:31:06 +0300

    EntityFramework

---
+++ dotNet/04-03-23/Entity_Lesson1/Entity_Lesson1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Lesson1
+{
+	internal class Program
+	{
+
+		public static SqlConnection con;
+
+		static void Main(string[] args)
+		{
+			//Klasik ADO.NET ile veritabanı erişimi sağlamak
+			con = new SqlConnection("Server=DESKTOP-OFVK2FD;Database=NORTHWND;Trusted_Connection=True;");
+
+			SqlCommand cmd = new SqlCommand("SELECT * FROM Employees", con);
+			con.Open();
+			SqlDataReader dr = cmd.ExecuteReader();
+			while (dr.Read())
+			{
+				Console.WriteLine($"{dr["FirstName"]} {dr["LastName"]}");
+			}
+			con.Close();
+			Console.ReadKey();
+
+			//ORM ile aynı kodun yazım biçimi
+			//NORTHWNDContext context = new();
+			//var employees = context.employees.ToList();
+		}
+	}
+}
