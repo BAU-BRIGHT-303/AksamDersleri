9199366f698b824e67424cfd51429e2f73fbed06
ulkuBerkay <116087678+ulkuBerkay@users.noreply.github.com>
2023-03-08 12:53:28 +0300

    ZarUpdate

--- Zar_Deneme/Zar_Deneme/Form1.cs
+++ Zar_Deneme/Zar_Deneme/Form1.cs
@@ -2,21 +2,16 @@ namespace Zar_Deneme
 {
     public partial class Form1 : Form
     {
+     public static   double baslangýc_butce = 100;
         public Form1()
         {
             InitializeComponent();
-
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             timer1.Start();
-
-           
         }
-
-        
 
         private void timer1_Tick(object sender, EventArgs e)
         {
@@ -26,70 +21,50 @@ namespace Zar_Deneme
             zar2 = rnd.Next(0, 6);
             pictureBox1.Image = ýmageList1.Images[zar1];
             pictureBox2.Image = ýmageList1.Images[zar2];
-
-
-
-
             int zar1Son, zar2Son;
-
             zar1Son = zar1 + 1;
             zar2Son = zar2 + 1;
-            int zar_toplam = zar1 + zar2;
-            int baslangýc_butce = 100;
-
+            double zar_toplam = zar1 + zar2;
             label8.Text = Convert.ToString(baslangýc_butce);
             if (baslangýc_butce >= 0)
             {
+                label1.Text = Convert.ToString(zar1Son);
+                label2.Text = Convert.ToString(zar2Son);
+                label3.Text = Convert.ToString(zar1Son + zar2Son);
                 if (zar_toplam == 12 || zar_toplam == 10 || zar_toplam == 6)
                 {
-
-
-                    label8.Text = Convert.ToString(baslangýc_butce * 3);
+                    baslangýc_butce = baslangýc_butce * 3;
+                    label8.Text = Convert.ToString(baslangýc_butce);
                     label5.Text = "TEBRÝKLER KAZANDINIZ";
-
-
+                    timer1.Stop();
+                    if (baslangýc_butce < 1)
+                    {
+                        MessageBox.Show("BÝTTÝ");
+                        timer1.Stop();
+                        button1.Visible = false;
+                    }
+                    
                 }
                 else if (zar_toplam == 8 || zar_toplam == 4 || zar_toplam == 2)
                 {
-
-
-                    label8.Text = Convert.ToString(baslangýc_butce / 2);
+                    baslangýc_butce = baslangýc_butce / 2;
+                    label8.Text = Convert.ToString(baslangýc_butce);
                     label5.Text = "2 ZARA 100.000'LÝK OLDUNUZ...";
-
-
+                    timer1.Stop();
+                    if (baslangýc_butce < 1)
+                    {
+                        MessageBox.Show("BÝTTÝ");
+                        timer1.Stop();
+                        button1.Visible = false;
+                    }
                 }
                 else
                 {
                     label5.Text = "Berabere";
-
+                    timer1.Stop(); 
                 }
-
-            }
-            else if  (baslangýc_butce <= 0)
-            {
-                MessageBox.Show("BÝTTÝ");
-            }
-
-            int sayac = 0;
-
-            sayac++;
-
-            if (sayac == 10)
-            {
-                timer1.Stop();
-                sayac = 0;
-
-
-
-                label1.Text = Convert.ToString(zar1Son);
-                label2.Text = Convert.ToString(zar2Son);
-                label3.Text = Convert.ToString(zar1Son + zar2Son);
-
-
-            }
+            }    
         }
-
-
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
